0806c8088e1829dd41ccfdc725115943f58c3c6c
M Usman <usmanali730771@gmail.com>
2024-10-14 16:57:49 +0500

    first commit

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Usman's Portfolio",
+  description: "WE DESIGN YOUR FEELINGS' CODE EMOTIONS",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
